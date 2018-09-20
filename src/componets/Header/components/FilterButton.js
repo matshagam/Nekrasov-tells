e31f7beffa66aec270877f8e7543646c7e65a98f
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-09-20 21:46:25 +0300

    Переписал класс компонент в полностью функциональный. Урра!

--- src/componets/Header/components/FilterButton.js
+++ src/componets/Header/components/FilterButton.js
@@ -4,12 +4,11 @@ import { connect } from 'react-redux';
 
 import { changeFilter } from '../../../actions/FilterAction';
 import { getBooks } from '../../../actions/BookAction';
-class FilterButton extends React.Component {
-  state = {
-    filter: 1
-  };
 
-  handleFilterChange = event => {
+const FilterButton = ({ getBooks, changeFilter, search, filters }) => {
+  let filter = 1;
+
+  const handleFilterChange = event => {
     let filterEvent = event.target.innerHTML;
 
     filterEvent =
@@ -21,46 +20,101 @@ class FilterButton extends React.Component {
             ? 2
             : null;
 
-    this.props.getBooks(this.props.search.query, filterEvent);
-    this.props.changeFilter(filterEvent);
-    this.setState({
-      filter: +event.target.getAttribute('id')
-    });
+    getBooks(search.query, filterEvent);
+    changeFilter(filterEvent);
+    filter = +event.target.getAttribute('id');
   };
 
-  renderButtons = () => {
-    const { filter } = this.state;
-    const FILTER = [
-      { id: 1, name: 'Везде' },
-      { id: 2, name: 'Книги' },
-      { id: 3, name: 'Периодика' }
-    ];
+  filter =
+    filters.filter === ''
+      ? 1
+      : filters.filter === 1
+        ? 2
+        : filters.filter === 2
+          ? 3
+          : null;
 
-    return (
-      <React.Fragment>
-        {FILTER.map(({ id, name }) => (
-          <button
-            disabled={id === filter ? true : false}
-            key={id}
-            id={id}
-            onClick={this.handleFilterChange}
-          >
-            {name}
-          </button>
-        ))}
-      </React.Fragment>
-    );
-  };
+  console.log('filter: ', filter);
 
-  render() {
-    console.log('in Header <Button/> render');
+  const FILTER = [
+    { id: 1, name: 'Везде' },
+    { id: 2, name: 'Книги' },
+    { id: 3, name: 'Периодика' }
+  ];
+  console.log('in Header <Button/> render');
 
-    return this.renderButtons();
-  }
-}
+  return FILTER.map(({ id, name }) => (
+    <button
+      disabled={id === filter ? true : false}
+      key={id}
+      id={id}
+      onClick={event => {
+        handleFilterChange(event);
+      }}
+    >
+      {name}
+    </button>
+  ));
+};
+
+// class FilterButton extends React.Component {
+//   state = {
+//     filter: 1
+//   };
+
+//   handleFilterChange = event => {
+//     let filterEvent = event.target.innerHTML;
+
+//     filterEvent =
+//       filterEvent === 'Везде'
+//         ? ''
+//         : filterEvent === 'Книги'
+//           ? 1
+//           : filterEvent === 'Периодика'
+//             ? 2
+//             : null;
+
+//     this.props.getBooks(this.props.search.query, filterEvent);
+//     this.props.changeFilter(filterEvent);
+//     this.setState({
+//       filter: +event.target.getAttribute('id')
+//     });
+//   };
+
+//   renderButtons = () => {
+//     const { filter } = this.state;
+//     const FILTER = [
+//       { id: 1, name: 'Везде' },
+//       { id: 2, name: 'Книги' },
+//       { id: 3, name: 'Периодика' }
+//     ];
+
+//     return (
+//       <React.Fragment>
+//         {FILTER.map(({ id, name }) => (
+//           <button
+//             disabled={id === filter ? true : false}
+//             key={id}
+//             id={id}
+//             onClick={this.handleFilterChange}
+//           >
+//             {name}
+//           </button>
+//         ))}
+//       </React.Fragment>
+//     );
+//   };
+
+//   render() {
+//     console.log('in Header <Button/> render');
+
+//     return this.renderButtons();
+//   }
+// }
 
 const mapStateToProps = store => ({
-  search: store.query
+  search: store.query,
+  filters: store.filter
 });
 
 export default connect(
