0d7eb21afefd80a20332fdb049e7a8abf76cd11a
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-09-20 22:10:36 +0300

    Some code optimization

--- src/componets/ToggleView/components/ToggleButton.js
+++ src/componets/ToggleView/components/ToggleButton.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 
 import { changeView } from '../../../actions/ViewAction';
 
-const ToggleButton = ({ changeView, views }) => {
-  let view = 2;
+const ToggleButton = ({ changeView, view }) => {
+  let listView = 2;
 
   const onClickChangeView = event => {
     let viewEvent = event.target.innerHTML;
@@ -31,13 +31,13 @@ const ToggleButton = ({ changeView, views }) => {
     }
   };
 
-  view = views.listView ? 1 : 2;
+  listView = view.listView ? 1 : 2;
 
   const VIEW_MODE = [{ id: 1, name: 'Список' }, { id: 2, name: 'Плитка' }];
 
   return VIEW_MODE.map(({ id, name }) => (
     <button
-      disabled={id === view ? true : false}
+      disabled={id === listView ? true : false}
       onClick={event => onClickChangeView(event)}
       id={id}
       key={id}
@@ -48,7 +48,7 @@ const ToggleButton = ({ changeView, views }) => {
 };
 
 const mapStateToProps = store => ({
-  views: store.listView
+  view: store.listView
 });
 
 export default connect(
